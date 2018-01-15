14464d780330ff0fe19b0cce44f10a5cba91f713
Michal Kalas <mikikalas@gmail.comgit config --global user.name mikepolak5>
2018-01-15 04:49:25 +0100

    15.3

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,14 +1,17 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
-    Task findById(long taskId);
+
+    @Override
+    Task save (Task task);
+
+    @Override
+    Optional<Task> findById(Long id);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,12 +1,12 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,7 +17,12 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
-    public Task getTaskById(long taskId) {
-        return repository.findById(taskId);
+
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
     }
 }
