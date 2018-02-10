3bbffb61e7808dcfe893a98c48753e555404ded6
Michal Kalas <mikikalas@gmail.comgit config --global user.name mikepolak5>
2018-02-10 17:05:48 +0100

    18.2

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
