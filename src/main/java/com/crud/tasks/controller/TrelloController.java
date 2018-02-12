c3aa63dc0ade725e6381ca5d9e54f3b63cab158d
Michal Kalas <mikikalas@gmail.comgit config --global user.name mikepolak5>
2018-02-12 19:04:37 +0100

    18.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -25,16 +25,18 @@ public class TrelloController {
         // GET request
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> {
+        trelloBoards.stream()
+                .filter(b -> b.getName().toUpperCase().contains("KODILLA"))
+                .forEach(trelloBoardDto -> {
 
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+                    System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
 
-            System.out.println("This board contains lists: ");
+                    System.out.println("This board contains lists: ");
 
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+                    trelloBoardDto.getLists().forEach(trelloList ->
+                            System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
 
-        });
+                });
 
     }
 
--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TrelloClient {
@@ -26,24 +27,28 @@ public class TrelloClient {
 
     @Value("${trello.app.token}")
     private String trelloToken;
+    @Value("${trello.app.username")
+    private String trelloUsername;
 
     @Autowired
     private RestTemplate restTemplate;
 
-    public List<TrelloBoardDto> getTrelloBoards() {
-
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/mikepolak5/boards")
+    private URI getUrl () {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("lists", "all")
                 .queryParam("fields", "name,id").build().encode().toUri();
+    }
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        URI url = getUrl();
 
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        boardsResponse = Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]);
 
-        if (boardsResponse != null) {
-            return Arrays.asList(boardsResponse);
-        }
-        return new ArrayList<>();
+        return Arrays.asList(boardsResponse);
     }
 
     public CreatedTrelloCard createNewCard(TrelloCardDto trelloCardDto) {
